a9aab7c311036a3b807036d610a372d373aa98d8
Michał Weinert <michal@weinert.io>
2022-04-11 21:34:08 +0200

    entrypoint tweaks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,12 +10,14 @@ elif [[ "${1}" == "freshclam" ]]; then
     if [[ "${WAIT_FOR_CLAMD}" == "true" ]]; then
         echo "Waiting for clamd on ${CLAMD_HOST}:${CLAMD_PORT}"
         waitforit -host=${CLAMD_HOST} -port=${CLAMD_PORT} -timeout=300
+        echo ${CLAMD_HOST} is up
     fi
     exec freshclam --daemon --foreground --checks="${FRESHCLAM_CHECKS}"
 elif [[ "${1}" == "api" ]]; then
     if [[ "${WAIT_FOR_CLAMD}" == "true" ]]; then
         echo "Waiting for clamd on ${CLAMD_HOST}:${CLAMD_PORT}"
         waitforit -host=${CLAMD_HOST} -port=${CLAMD_PORT} -timeout=300
+        echo ${CLAMD_HOST} is up
     fi
     exec gunicorn -c gunicorn.py api:app
 else
